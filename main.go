c13714083efdc4bd933efbe345c5375f4f1375dc
brenogmrs <brenoaraujo@outlook.com.br>
2023-01-22 17:04:48 -0300

    feat:add and implement logger

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/brenogmrs/go-crud/src/configuration/logger"
 	"github.com/brenogmrs/go-crud/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-
+	logger.Info("Starting Application")
 	err := godotenv.Load()
 
 	if err != nil {
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,24 +1,44 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/brenogmrs/go-crud/src/configuration/logger"
 	"github.com/brenogmrs/go-crud/src/configuration/validation"
 	"github.com/brenogmrs/go-crud/src/controller/model/request"
+	"github.com/brenogmrs/go-crud/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller", 
+		zap.String("journey", "create-user"),
+	)
 
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restError := validation.ValidateUserError(err)
+		logger.Error("Error validating user info", err,
+			zap.String("journey", "create-user"),
+		)
 
+		restError := validation.ValidateUserError(err)
 		c.JSON(restError.Code, restError)
 		return
 	}
+	
+	
+	response := response.UserResponse{
+		ID: "teste",
+		Email: userRequest.Email,
+		Name: userRequest.Name,
+		Age: userRequest.Age,
+	}
 
-	fmt.Println(userRequest)
+	logger.Info("User created successfully",
+		zap.String("journey", "create-user"),
+	)
 
+	c.JSON(http.StatusOK, response)
 }
